b6f307de0efbc001265b0fe0f394e2be605e430a
Will Colton <will.colton@hp.com>
2015-03-24 21:30:01 -0400

    Created a very basic base project based on my previous projects and the koa todo example.

---
+++ controllers/home.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var config = require('../config');
+var path = require('path');
+var fs = require('fs');
+
+var page = fs.readFileSync(path.join(__dirname, '../views/index.html'), 'utf8')
+  .replace('{{version}}', config.version);
+
+module.exports = function* home(next) {
+  this.body = page;
+};
