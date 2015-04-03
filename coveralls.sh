1ea72d92fd93deeac5a908304854dabd69e202a1
Will Colton <will.colton@hp.com>
2015-04-02 23:09:17 -0400

    Fix some blatant bugs in my coveralls configuration.

--- coveralls.sh
+++ coveralls.sh
@@ -6,7 +6,7 @@ pwd
 
 # Piping directly from node was only sending one line for some reason
 # so I create a file and then cat it which works
-iojs ./node_modules/.bin/_mocha -R mocha-lcov-reporter > coverage.lcov
+iojs ./node_modules/mocha/bin/_mocha test/** -R mocha-lcov-reporter > coverage.lcov
 cat coverage.lcov | iojs ./node_modules/coveralls/bin/coveralls.js
 
 # always exit success, build should not depend on coveralls
