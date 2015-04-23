87a80d01e31aacf3a19ce9d1e837ec03f96f01dd
Will Colton <coltonw@gmail.com>
2015-04-23 07:49:09 -0400

    Add a static version of create user form.

--- public/jsx/revonarchy.jsx
+++ public/jsx/revonarchy.jsx
@@ -1,19 +1,51 @@
-var Application = React.createClass({
-  getInitialState: function() {
-    return {
-      content: []
-    };
-  },
+var Application = function() {
 
-  render: function() {
-    var PageHeader = ReactBootstrap.PageHeader;
-    return (
-      <div className="container">
-        <PageHeader>Revonarchy <small>Who will rule this day?</small></PageHeader>
-      </div>
-    );
-  }
+  var CreateUser = React.createClass({
+    render: function() {
+      var Input = ReactBootstrap.Input;
+      return (
+        <form>
+          <Input
+            type='text'
+            placeholder='Enter email'
+            label='Email Address'
+            value={this.props.userEmail}
+            ref='userEmailTextInput'
+          />
+          <Input
+            type='text'
+            placeholder='Enter Nickname'
+            label='Nickname'
+            value={this.props.userNickname}
+            ref='userNicknameInput'
+          />
+          <Input type='submit' value='Create User' />
+        </form>
+      );
+    }
 });
+
+
+  var Application = React.createClass({
+    getInitialState: function() {
+      return {
+        content: []
+      };
+    },
+
+    render: function() {
+      var PageHeader = ReactBootstrap.PageHeader;
+      return (
+        <div className='container'>
+          <PageHeader>Revonarchy <small>Who will rule this day?</small></PageHeader>
+          <CreateUser userEmail={''} userNickname={''} />
+        </div>
+      );
+    }
+  });
+
+  return Application;
+}();
 
 $(document).ready(function() {
   React.render(<Application />, document.body);
