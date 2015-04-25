1e9db2eb97b6bb5007276742a4676f54261d2a2e
Will Colton <coltonw@gmail.com>
2015-04-25 10:31:38 -0400

    Create user form is now functional.

--- public/jsx/revonarchy.jsx
+++ public/jsx/revonarchy.jsx
@@ -1,34 +1,81 @@
 var Application = function() {
 
   var CreateUser = React.createClass({
+    handleChange: function() {
+      this.props.onUserInput(
+        this.refs.userEmailTextInput.getValue(),
+        this.refs.userNicknameInput.getValue()
+      );
+    },
+
     render: function() {
       var Input = ReactBootstrap.Input;
       return (
-        <form>
+        <form onSubmit={this.props.onSubmit} >
           <Input
             type='text'
             placeholder='Enter email'
             label='Email Address'
+            value={this.props.email}
+            onChange={this.handleChange}
             ref='userEmailTextInput'
           />
           <Input
             type='text'
             placeholder='Enter Nickname'
             label='Nickname'
+            value={this.props.nickname}
+            onChange={this.handleChange}
             ref='userNicknameInput'
           />
           <Input type='submit' value='Create User' />
         </form>
       );
     }
-});
+  });
 
 
   var Application = React.createClass({
     getInitialState: function() {
       return {
-        content: []
+        createUser: {
+          email: '',
+          nickname: ''
+        },
+        users: []
       };
+    },
+
+    onUserInput: function(email, nickname) {
+      this.setState({
+        createUser: {
+          email: email,
+          nickname: nickname
+        }
+      });
+    },
+
+    createUser: function(event) {
+      var self = this;
+      $.ajax('/user', {
+        method: 'post',
+        data: JSON.stringify({
+          user: {
+            email: this.state.createUser.email,
+            name: this.state.createUser.nickname
+          }
+        }),
+        contentType: "application/json; charset=utf-8",
+        dataType: 'json',
+        success: function(data){
+          if(data && data.user) {
+            self.setState(self.state.users.concat(data.user));
+          }
+        },
+        failure: function(errMsg) {
+          //TODO Handle this error
+        }
+      });
     },
 
     render: function() {
@@ -36,11 +83,11 @@ var Application = function() {
       var PageHeader = ReactBootstrap.PageHeader;
       return (
         <div>
-          <Navbar staticTop='true'>
+          <Navbar staticTop={true}>
             <h1>REVONARCHY <small>Who will rule this day?</small></h1>
           </Navbar>
           <div className='container'>
-            <CreateUser userEmail={''} userNickname={''} />
+            <CreateUser email={this.state.email} nickname={this.state.nickname} onUserInput={this.onUserInput} onSubmit={this.createUser} />
           </div>
         </div>
       );
