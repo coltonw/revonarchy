6fdbcd2013263fe623f879f7c286d26ca3b10826
Will Colton <coltonw@gmail.com>
2015-05-17 18:40:48 -0400

    Update coveralls script so that my shiny coverage badge updates to reflect my new models tests.

--- coveralls.sh
+++ coveralls.sh
@@ -6,7 +6,7 @@ pwd
 
 # Piping directly from node was only sending one line for some reason
 # so I create a file and then cat it which works
-iojs ./node_modules/istanbul-harmony/lib/cli.js cover ./node_modules/mocha/bin/_mocha test/controllers
+iojs ./node_modules/istanbul-harmony/lib/cli.js cover ./node_modules/mocha/bin/_mocha test/controllers test/models
 cat coverage/lcov.info | iojs ./node_modules/coveralls/bin/coveralls.js
 
 # always exit success, build should not depend on coveralls
