9f975bcefcb2bf0d6a5140e373ab1bf83bbf5810
Will Colton <coltonw@gmail.com>
2015-05-27 07:46:38 -0400

    Added functionality for query params.

--- public/jsx/revonarchy.jsx
+++ public/jsx/revonarchy.jsx
@@ -274,17 +274,84 @@ var Application = (function() {
     }
   });
 
+  // Copyright (c) Sindre Sorhus <sindresorhus@gmail.com> (sindresorhus.com)
+  // https://github.com/sindresorhus/query-string/blob/master/license
+  var getQueryTerms = function () {
+    var str = window.location.search;
+    if (typeof str !== 'string') {
+      return {};
+    }
+
+    str = str.trim().replace(/^(\?|#|&)/, '');
+
+    if (!str) {
+      return {};
+    }
+
+    return str.trim().split('&').reduce(function (ret, param) {
+      var parts = param.replace(/\+/g, ' ').split('=');
+      var key = parts[0];
+      var val = parts[1];
+
+      key = decodeURIComponent(key);
+      // missing `=` should be `null`:
+      // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
+      val = val === undefined ? null : decodeURIComponent(val);
+
+      if (!ret.hasOwnProperty(key)) {
+        ret[key] = val;
+      } else if (Array.isArray(ret[key])) {
+        ret[key].push(val);
+      } else {
+        ret[key] = [ret[key], val];
+      }
+
+      return ret;
+    }, {});
+  };
+
   var getSavedUsers = function() {
     var locallyStoredUsers;
     try {
       locallyStoredUsers = JSON.parse(localStorage.users);
-      if ($.isArray(locallyStoredUsers)) {
-        return locallyStoredUsers;
-      } else {
-        return [];
+      if (!$.isArray(locallyStoredUsers)) {
+        locallyStoredUsers = [];
       }
     } catch (e) {
-      return [];
+      locallyStoredUsers = [];
+    }
+
+    var queryUsers;
+    var queryUser;
+    var i;
+    var j;
+    var found;
+    queryUsers = getQueryTerms().user;
+    if (queryUsers) {
+      if (!Array.isArray(queryUsers)) {
+        queryUsers = [queryUsers];
+      }
+      for (i = 0; i < queryUsers.length; i++) {
+        try {
+          queryUser = JSON.parse(queryUsers[i]);
+          found = false;
+          for (j = 0; j < locallyStoredUsers.length; j++) {
+            if (queryUser.email === locallyStoredUsers[j].email) {
+              locallyStoredUsers[j].name = queryUser.name;
+              found = true;
+            }
+          }
+          if (!found) {
+            locallyStoredUsers.push({
+              email: queryUser.email,
+              name: queryUser.name
+            });
+          }
+        } catch (e) {}
+      }
+      return locallyStoredUsers;
+    } else {
+      return locallyStoredUsers;
     }
   };
 
